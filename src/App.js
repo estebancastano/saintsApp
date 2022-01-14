c335e02111f657f114964bb6c85e535b2948e26c
Esteban <estebin5372@gmail.com>
2022-01-14 15:04:17 -0500

    Se eliminó bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './components/NavbarComp';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <NavbarComp/>
     </div>
   );
 }
---
+++ src/components/NavbarComp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import React from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+export default class NavbarComp extends Component {
+    render() {
+        return (
+            <Router>
+              
+            </Router>
+        )
+    }
+}
