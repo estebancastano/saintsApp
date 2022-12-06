e1fb2562042cb50a7ab22843094e0055e0275621
Esteban <estebin5372@gmail.com>
2022-12-06 15:46:39 -0500

    repositorio de saints-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Vidas de Santos</h1>
+      <div className="accordion">
+        some item
+      </div>
     </div>
   );
 }
